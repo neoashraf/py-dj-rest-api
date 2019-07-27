67e7570c3ec8dfa2f8896a69b4f543fb586fcd7c
neo <ashrafuddin160@gmail.com>
2019-07-28 02:06:19 +0600

    Added a custom user manger

--- app/profiles/models.py
+++ app/profiles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-
-
+from django.contrib.auth.models import BaseUserManger
 
 # Create your models here.
 class UserProfile(AbstractBaseUser, PermissionsMixin):
+    
     """Represents a user profile in our system"""
     
     email = models.EmailField(max_length=255, unique=True)
@@ -29,3 +29,34 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str___(self)
         """Django uses this when it needs to convert the object to a string"""    
         return self.email
+    
+    
+class UserProfileManager(BAseUserManager):
+    """Helps dgango work with our custom user model"""
+    
+    def create_user(self, email, name, password=none):
+        """Creates new user profile object."""
+    
+        if not email:
+            raise VAlueError('User must have an email address.')
+        
+        email = self.normalize_email(email)
+        user  = self.model(email= email, name=name)
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with the given details."""
+        
+        user = self.create_user(email, name, password)
+        
+        user.is_superuser = True
+        user.is_staff = True
+        
+        user.save(using=self._db)
+        
+        return user
+    

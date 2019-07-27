5724743a7917a82ee9eb2eaad6e74cf94944278d
neo <ashrafuddin160@gmail.com>
2019-07-27 10:26:50 +0600

    added new property in model, changed some previous types

--- app/products/models.py
+++ app/products/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title             = models.TextField()
-    description       = models.TextField()
-    price             = models.TextField()
+    title             = models.CharField(max_length = 20)
+    description       = models.TextField(blank=True, null=True)
+    price             = models.DecimalField(decimal_places=2, max_digits=100)
+    summary           = models.TextField()
+    featuredId        = models.BooleanField()
+    rating            = models.DecimalField(decimal_places=2, max_digits=3,null=True)

e70690a85dc6d9b46855e9f262049834a3581d7e
neo <ashrafuddin160@gmail.com>
2019-08-04 06:13:03 +0600

    added a permission class, added it to userprofile viewset and also added authentication class with it

--- app/profiles/views.py
+++ app/profiles/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
-from rest_framework import viewsets
 
 from . import serializers
 from . import models
+from . import permissions
+
 
 # Create your views here.
 
@@ -108,3 +111,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     
     serializer_class = serializers.UserProfileSerializer 
     queryset = models.UserProfile.objects.all()
+    
+    """ Add authentication and permission class to a viewset"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    

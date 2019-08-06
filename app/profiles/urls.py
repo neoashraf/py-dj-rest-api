2b09a985bbedde324b8fed83e01e8c0d3ab82a10
neo <ashrafuddin160@gmail.com>
2019-08-07 00:36:24 +0600

    loginviewset code uncommented, previously it did not work because of not running migaration command

--- app/profiles/urls.py
+++ app/profiles/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
-from django.urls import path, include
+# from django.urls import path
+from django.urls import include
+
 
 from rest_framework.routers import DefaultRouter
 
@@ -8,11 +10,11 @@ from . import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)       # it is a modelviewset 
-# router.register('login', views.LoginViewSet, base_name='login')
+router.register('login', views.LoginViewSet, base_name='login')
 
 urlpatterns = [
     url(r'^hello-apiview/', views.HelloApiView.as_view()),
-    path('login/', views.UserLoginApiView.as_view()),
+    # path('login/', views.UserLoginApiView.as_view()),
     url(r'',include(router.urls)),
 ]
 

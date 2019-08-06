4081ba335909ffb347337d1be3f13fd43da4a28f
neo <ashrafuddin160@gmail.com>
2019-08-07 02:25:11 +0600

    added viewset for profilefeeditem viewset, added url for it

--- app/profiles/urls.py
+++ app/profiles/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 # from django.urls import path
 from django.urls import include
 
-
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -11,6 +10,7 @@ router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)       # it is a modelviewset 
 router.register('login', views.LoginViewSet, base_name='login')
+router.register('feed', views.ProfileFeedItemViewSet)      # it is a modelviewset 
 
 urlpatterns = [
     url(r'^hello-apiview/', views.HelloApiView.as_view()),
--- app/profiles/views.py
+++ app/profiles/views.py
@@ -137,3 +137,13 @@ class LoginViewSet(viewsets.ViewSet):
 #     """Handle creating user authentication tokens"""
 #     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     
+class ProfileFeedItemViewSet(viewsets.ModelViewSet):
+    """ Handles creating, reading and updating profile feed items """
+    
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    
+    def perform_create(self, serializer):
+        """ Sets the user profile to the logged in user. """
+        serializer.save(user_profile=self.request.user)

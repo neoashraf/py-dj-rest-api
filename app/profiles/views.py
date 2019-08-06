a35348dc6cbd9385116c75c6c980ea888fec9d1a
neo <ashrafuddin160@gmail.com>
2019-08-07 03:52:22 +0600

    added permission class and applied to profilefeeditem viewset, now only loggedin user can post and user can edit their posts only

--- app/profiles/views.py
+++ app/profiles/views.py
@@ -9,7 +9,7 @@ from rest_framework import filters
 # from rest_framework.settings import api_settings
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from . import serializers
 from . import models
@@ -143,6 +143,7 @@ class ProfileFeedItemViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
     
     def perform_create(self, serializer):
         """ Sets the user profile to the logged in user. """
